609ad80739353de75996152fd51d183a0a33a588
17sqrt2 <17pavels@gmail.com>
2019-07-09 18:37:32 +0500

    HW5 fix after 1st review

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +12,6 @@ import java.util.List;
 @Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     @Transactional
-    @Modifying
     int deleteMealByIdAndUserId(int id, int userId);
 
     List<Meal> getAllByUserIdAndDateTimeBetweenOrderByDateTimeDesc(
