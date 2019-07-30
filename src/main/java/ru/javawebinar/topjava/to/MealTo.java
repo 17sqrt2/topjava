5b9f17f5fea06dbb2cf92a9ab22db1839580f48f
17sqrt2 <17pavels@gmail.com>
2019-07-31 00:55:32 +0500

    HW7 without optional

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.to;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDateTime;
 
 public class MealTo {
@@ -13,7 +16,13 @@ public class MealTo {
 
     private final boolean excess;
 
-    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+    @JsonCreator
+    public MealTo(
+            @JsonProperty("id") Integer id,
+            @JsonProperty("dateTime") LocalDateTime dateTime,
+            @JsonProperty("description") String description,
+            @JsonProperty("calories") int calories,
+            @JsonProperty("excess") boolean excess) {
         this.id = id;
         this.dateTime = dateTime;
         this.description = description;
@@ -50,5 +59,17 @@ public class MealTo {
                 ", calories=" + calories +
                 ", excess=" + excess +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        MealTo mealTo = (MealTo) obj;
+        return this.getId().equals(mealTo.getId())
+                && this.getDateTime().equals(mealTo.getDateTime())
+                && this.getDescription().equals(mealTo.getDescription())
+                && this.getCalories() == mealTo.getCalories()
+                && this.isExcess() == mealTo.isExcess();
     }
 }
